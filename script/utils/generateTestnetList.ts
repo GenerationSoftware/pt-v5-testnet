01a8fef41ad411f6b46a9775b992f4737a65c4ff
Brendan Asselstine <mail.asselstine@gmail.com>
2023-08-04 12:48:11 -0700

    Deployed to Sepolia

--- script/utils/generateTestnetList.ts
+++ script/utils/generateTestnetList.ts
@@ -5,29 +5,29 @@ import {
   writeList,
 } from "../helpers/generateContractList";
 
-const ethGoerliStableTokenDeploymentPath = `${rootFolder}/broadcast/DeployStableToken.s.sol/5`;
-const ethGoerliTokenDeploymentPath = `${rootFolder}/broadcast/DeployToken.s.sol/5`;
-const ethGoerliVaultDeploymentPath = `${rootFolder}/broadcast/DeployVault.s.sol/5`;
+// const ethGoerliStableTokenDeploymentPath = `${rootFolder}/broadcast/DeployStableToken.s.sol/5`;
+// const ethGoerliTokenDeploymentPath = `${rootFolder}/broadcast/DeployToken.s.sol/5`;
+// const ethGoerliVaultDeploymentPath = `${rootFolder}/broadcast/DeployVault.s.sol/5`;
 
-const ethGoerliDeploymentPaths = [
-  ethGoerliStableTokenDeploymentPath,
-  ethGoerliTokenDeploymentPath,
-  `${rootFolder}/broadcast/DeployPool.s.sol/5`,
-  `${rootFolder}/broadcast/DeployYieldVault.s.sol/5`,
-  ethGoerliVaultDeploymentPath,
-];
+// const ethGoerliDeploymentPaths = [
+//   ethGoerliStableTokenDeploymentPath,
+//   ethGoerliTokenDeploymentPath,
+//   `${rootFolder}/broadcast/DeployPool.s.sol/5`,
+//   `${rootFolder}/broadcast/DeployYieldVault.s.sol/5`,
+//   ethGoerliVaultDeploymentPath,
+// ];
 
-const ethGoerliTokenDeploymentPaths = [
-  ethGoerliStableTokenDeploymentPath,
-  ethGoerliTokenDeploymentPath,
-];
+// const ethGoerliTokenDeploymentPaths = [
+//   ethGoerliStableTokenDeploymentPath,
+//   ethGoerliTokenDeploymentPath,
+// ];
 
-writeList(generateContractList(ethGoerliDeploymentPaths), "deployments/ethGoerli", "contracts");
-writeList(
-  generateVaultList(ethGoerliVaultDeploymentPath, ethGoerliTokenDeploymentPaths),
-  "deployments/ethGoerli",
-  "vaults"
-);
+// writeList(generateContractList(ethGoerliDeploymentPaths), "deployments/ethGoerli", "contracts");
+// writeList(
+//   generateVaultList(ethGoerliVaultDeploymentPath, ethGoerliTokenDeploymentPaths),
+//   "deployments/ethGoerli",
+//   "vaults"
+// );
 
 const ethSepoliaStableTokenDeploymentPath = `${rootFolder}/broadcast/DeployStableToken.s.sol/11155111`;
 const ethSepoliaTokenDeploymentPath = `${rootFolder}/broadcast/DeployToken.s.sol/11155111`;
@@ -53,23 +53,23 @@ writeList(
   "vaults"
 );
 
-const mumbaiStableTokenDeploymentPath = `${rootFolder}/broadcast/DeployStableToken.s.sol/80001`;
-const mumbaiTokenDeploymentPath = `${rootFolder}/broadcast/DeployToken.s.sol/80001`;
-const mumbaiVaultDeploymentPath = `${rootFolder}/broadcast/DeployVault.s.sol/80001`;
+// const mumbaiStableTokenDeploymentPath = `${rootFolder}/broadcast/DeployStableToken.s.sol/80001`;
+// const mumbaiTokenDeploymentPath = `${rootFolder}/broadcast/DeployToken.s.sol/80001`;
+// const mumbaiVaultDeploymentPath = `${rootFolder}/broadcast/DeployVault.s.sol/80001`;
 
-const mumbaiDeploymentPaths = [
-  mumbaiStableTokenDeploymentPath,
-  mumbaiTokenDeploymentPath,
-  `${rootFolder}/broadcast/DeployPool.s.sol/80001`,
-  `${rootFolder}/broadcast/DeployYieldVault.s.sol/80001`,
-  mumbaiVaultDeploymentPath,
-];
+// const mumbaiDeploymentPaths = [
+//   mumbaiStableTokenDeploymentPath,
+//   mumbaiTokenDeploymentPath,
+//   `${rootFolder}/broadcast/DeployPool.s.sol/80001`,
+//   `${rootFolder}/broadcast/DeployYieldVault.s.sol/80001`,
+//   mumbaiVaultDeploymentPath,
+// ];
 
-const mumbaiTokenDeploymentPaths = [mumbaiStableTokenDeploymentPath, mumbaiTokenDeploymentPath];
+// const mumbaiTokenDeploymentPaths = [mumbaiStableTokenDeploymentPath, mumbaiTokenDeploymentPath];
 
-writeList(generateContractList(mumbaiDeploymentPaths), "deployments/mumbai", "contracts");
-writeList(
-  generateVaultList(mumbaiVaultDeploymentPath, mumbaiTokenDeploymentPaths),
-  "deployments/mumbai",
-  "vaults"
-);
+// writeList(generateContractList(mumbaiDeploymentPaths), "deployments/mumbai", "contracts");
+// writeList(
+//   generateVaultList(mumbaiVaultDeploymentPath, mumbaiTokenDeploymentPaths),
+//   "deployments/mumbai",
+//   "vaults"
+// );
