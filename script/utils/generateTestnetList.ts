68c51277ed8196e27634ad8abd46ef99deeb251a
Trevor Richard <trmidpoint@gmail.com>
2024-08-30 16:40:54 +0000

    deploy to scroll sepolia

--- script/utils/generateTestnetList.ts
+++ script/utils/generateTestnetList.ts
@@ -20,4 +20,5 @@ writeFiles(rootFolder, getDeploymentPaths(11155420), "optimismSepolia", remap);
 writeFiles(rootFolder, getDeploymentPaths(84532), "baseSepolia", remap);
 writeFiles(rootFolder, getDeploymentPaths(421614), "arbitrumSepolia", remap);
 writeFiles(rootFolder, getDeploymentPaths(11155111), "sepolia", remap);
+writeFiles(rootFolder, getDeploymentPaths(534351), "scrollSepolia", remap);
 writeFiles(rootFolder, getDeploymentPaths(31337), "local", remap);
